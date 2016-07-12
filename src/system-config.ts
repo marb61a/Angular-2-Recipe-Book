1f0153f6a72528b36758a4946629825df61d5932
martin byrne <marb61a@gmail.com>
2016-07-12 13:23:34 +0000

    added system config file

---
+++ src/system-config.ts
@@ -0,0 +1,59 @@
+// USER CONFIGURATION
+
+// Map relative paths to URL
+const map: any = {
+};
+
+// User packages configuration
+const packages: any = {
+};
+
+
+// ALL UNDER THIS LINE IS MANAGED BY THE CLI
+const barrels: string[] = [
+  // Angular specific barrels.
+  '@angular/core',
+  '@angular/common',
+  '@angular/compiler',
+  '@angular/http',
+  '@angular/router',
+  '@angular/forms',
+  '@angular/platform-browser',
+  '@angular/platform-browser-dynamic',
+
+  // Thirdparty barrels.
+  'rxjs',
+
+  // App specific barrels.
+  'app',
+  'app/shared',
+  'app/recipes',
+  'app/recipes/recipe-list',
+  'app/recipes/recipe-detail',
+  'app/shopping-list',
+  'app/shopping-list/shopping-list-add',
+  'app/recipes/recipe-edit',
+  /** @cli-barrel */
+];
+
+const cliSystemConfigPackages: any = {};
+barrels.forEach((barrelName: string) => {
+  cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+
+/** Type declaration for ambient System. */
+declare var System: any;
+
+// Apply the CLI SystemJS configuration.
+System.config({
+  map: {
+    '@angular': 'vendor/@angular',
+    'rxjs': 'vendor/rxjs',
+    'main': 'main.js'
+  },
+  packages: cliSystemConfigPackages
+});
+
+// Apply the user's configuration.
+System.config({ map, packages });
+
