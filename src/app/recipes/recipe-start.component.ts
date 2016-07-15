dc0a587e8f7d703a66ecb749abb21e5c700344e2
martin byrne <marb61a@gmail.com>
2016-07-15 13:03:00 +0000

    added recipe start component

---
+++ src/app/recipes/recipe-start.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId : module.Id,
+    selector : 'rb-recipe-start',
+    template: `
+        <h1>Please select a Recipe</h1>
+    `,
+    styles: []
+})
+
+export class RecipeStartComponent{
+    
+}
