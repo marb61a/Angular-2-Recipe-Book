ac60c9e5e626fc21a190b64654471ebd3d867027
martin byrne <marb61a@gmail.com>
2016-07-20 10:10:49 +0000

    updated recipe routes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,6 +4,31 @@ import { Headers, Http, Response } from "@angular/http";
 import { Recipe } from "./recipe";
 import { Ingredient } from "../shared";
 
+@Injectable()
 export class RecipeService{
+    recipesCahnged = new EventEmitter<Recipe[]>();
     
+    private recipes : Recipe[] = []
+    
+    constructor(private http: Http) {}
+    
+    getRecipes(){
+        return this.recipes;
+    }
+    
+    getRecipe(id : number){
+        return this.recipes[id];
+    }
+    
+    deleteRecipe(recipe: Recipe){
+        this.recipes.splice(this.recipes.indexOf(recipe), 1);    
+    }
+    
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+    }
+    
+    editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
+        this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+    }
 }
