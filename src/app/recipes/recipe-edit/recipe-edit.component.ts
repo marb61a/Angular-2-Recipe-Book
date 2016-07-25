71633c2cb1a1a9b1bbf7a6ca569350647f89f159
martin byrne <marb61a@gmail.com>
2016-07-25 10:32:53 +0000

    updated recipe edit component

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -88,4 +88,38 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     private navigateBack() {
         this.router.navigate(['../']);
     }
+    
+    private initForm(){
+        let recipeName = '';
+        let recipeImageUrl = '';
+        let recipeContent = '';
+        let recipeIngredients: FormArray = new FormArray([]);
+        
+        if(!this.isNew){
+            if(this.recipe.hasOwnProperty('ingredients')){
+                for(let i = 0; i < this.recipe.ingredients.length; i++){
+                    recipeIngredients.push(
+                        new FormGroup({
+                            name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
+                            amount: new FormControl(this.recipe.ingredients[i].amount, [
+                            Validators.required,
+                            Validators.pattern("\\d+")
+                        ])
+                    })   
+                );    
+            }
+        }
+        
+        recipeName = this.recipe.name;
+        recipeImageUrl = this.recipe.imagePath;
+        recipeContent = this.recipe.description;
+    }
+    
+    this.recipeForm = this.formBuilder.group({
+        name: [recipeName, Validators.required],
+        imagePath: [recipeImageUrl, Validators.required],
+        description: [recipeContent, Validators.required],
+        ingredients: recipeIngredients    
+    })
+  }
 }
